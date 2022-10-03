7d6bccf37f4261a3d2bedd0b3a8bed8c02bf6fd3
Kevin <kevin.castro.chavez0@gmail.com>
2022-10-03 16:58:31 -0600

    installed router and created functions for authenticated routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,57 @@
+import {
+  Link,
+  Route,
+  BrowserRouter as Router,
+  Redirect,
+} from 'react-router-dom';
+import { AuthContextProvider, useAuthState } from './firebase';
+
 function App() {
+  const AuthenticatedRoute = ({ component: C, ...props }) => {
+    const { isAuthenticated } = useAuthState();
+    return (
+      <Route
+        {...props}
+        render={(routeProps) =>
+          isAuthenticated ? <C {...routeProps} /> : <Redirect to='/login' />
+        }
+      />
+    );
+  };
+
+  const UnauthenticatedRoute = ({ component: C, ...props }) => {
+    const { isAuthenticated } = useAuthState();
+    return (
+      <Route
+        {...props}
+        render={(routeProps) =>
+          !isAuthenticated ? <C {...routeProps} /> : <Redirect to='/' />
+        }
+      />
+    );
+  };
+
   return (
-    <div className='App'>
-      <h1>Hello</h1>
-    </div>
+    <AuthContextProvider>
+      <Router>
+        <div>
+          <Link to='/'>Home</Link> | <Link to='/login'>Login</Link> | {''}
+          <Link to='/signup'>Sign Up</Link>
+        </div>
+
+        <AuthenticatedRoute exact path='/' component={<h1>Home</h1>} />
+        <UnauthenticatedRoute
+          exact
+          path='/signup'
+          component={<h1>Sign Up</h1>}
+        />
+        <UnauthenticatedRoute
+          exact
+          path='/signin'
+          component={<h1>Sign In</h1>}
+        />
+      </Router>
+    </AuthContextProvider>
   );
 }
 
